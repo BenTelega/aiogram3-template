592b017616958317ce0283094c136f6adefa3b8a
Artem <stroganov400@gmail.com>
2023-02-06 19:05:21 +0400

    added filter on admins

--- src/handlers/admin/main.py
+++ src/handlers/admin/main.py
@@ -1,7 +1,15 @@
 from modules import * 
+from aiogram.filters import BaseFilter
 from utils import functions
 
 router = Router()
+
+
+class AdminFilter(BaseFilter): 
+    async def __call__(self, message: types.Message) -> bool:
+        pass
+
+router.message.filter(AdminFilter())
 
 @router.message(filters.Text('⬅️ Назад'))
 async def cmd_back(message: types.Message):
