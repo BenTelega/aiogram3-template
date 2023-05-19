05f006821a92fcab857ceef86e42adf1bc4ab849
Artem <stroganov400@gmail.com>
2023-05-19 14:52:29 +0400

    updated

--- src/handlers/admin/main.py
+++ src/handlers/admin/main.py
@@ -11,6 +11,10 @@ class AdminFilter(BaseFilter):
 
 router.message.filter(AdminFilter())
 
+@router.message(filters.Command("admin"))
+async def cmd_admin(message: types.Message):
+    await message.answer('<b>Админ-Панель</b>', reply_markup=ADMIN)
+
 @router.message(filters.Text('⬅️ Назад'))
 async def cmd_back(message: types.Message):
     await message.answer('<b>Меню</b>', reply_markup=MENU)
--- src/handlers/user/main.py
+++ src/handlers/user/main.py
@@ -10,7 +10,3 @@ async def cmd_start(message: types.Message):
     result = functions.check_start_command(message.text)
     if result['exist']:
         await functions.handler_start_command(message, result['data']['command'], result['data']['data'])
-
-@router.message(filters.Command("admin"), F.from_user.id.in_(db.json_config.get_value('admins')))
-async def cmd_admin(message: types.Message):
-    await message.answer('<b>Админ-Панель</b>', reply_markup=ADMIN)
